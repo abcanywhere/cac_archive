bfe35cf19874234b3ae812e00ba1c197379b573a
abcanywhere <nine327@gmail.com>
2025-10-09 16:30:08 +0800

    Automated backup on 2025-10-09 16:30:08

--- sqlbackup.sh
+++
@@ -1,4 +0,0 @@
-mysql -uappuser -pcac_br1dge -Dcaimdb < schedule.sql
-sudo mysqldump -u root --skip-extended-insert caimdb | gzip > caimdb.dump.gz
-# rsync caimdb.dump.gz cachk@aws9327.ddns.net:.
-rsync caimdb.dump.gz cachk@dreamtech.ddns.net:.
